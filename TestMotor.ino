ab4a160821a9ff4712eedda305f04b7869a9d84f
Bmttkiz <167805729+Bmttkiz@users.noreply.github.com>
2025-09-28 15:27:00 +0700

    Update TestMotor.ino

--- TestMotor.ino
+++ TestMotor.ino
@@ -6,12 +6,19 @@
 
 #include <Servo.h> 
 Servo esc; // Object from class
+Servo esc2;
+Servo esc3;
+Servo esc4;
 
 void setup() {
  
   esc.attach(6); //Set I/O PWM 
-  esc.writeMicroseconds(2000); //Set max pulse-width
-  esc.writeMicroseconds(1000); //Set min pulse-width
+  esc2.attach(6); //Set I/O PWM 
+  esc3.attach(6); //Set I/O PWM 
+  esc4.attach(6); //Set I/O PWM 
+ 
+  // esc.writeMicroseconds(2000); //Set max pulse-width
+  // esc.writeMicroseconds(1000); //Set min pulse-width
   
 }//end setup fuc
 
@@ -20,11 +27,17 @@ void loop() {
   for(int i = 1000; i <= 2000; i++)
   {
     esc.writeMicroseconds(i);
+    esc2.writeMicroseconds(i);
+    esc3.writeMicroseconds(i);
+    esc4.writeMicroseconds(i);
     delay(10);
   }
   for(int i = 2000; i >= 1000; i--)
   {
     esc.writeMicroseconds(i);
+    esc2.writeMicroseconds(i);
+    esc3.writeMicroseconds(i);
+    esc4.writeMicroseconds(i);
     delay(10);
   }
   delay(1000);
